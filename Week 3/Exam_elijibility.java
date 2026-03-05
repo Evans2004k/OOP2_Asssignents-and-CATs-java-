aaaeab8a9fc3a84843e2ca5a5748660753085f56
Evans mawira <evansmawira2004@gmail.com>
2026-03-05 13:41:21 +0300

    Added week 3 Assignment 1

---
+++ Week 3/Exam_elijibility.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exam_elijibility{
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter student attendance (%): ");
+        double attendance = scanner.nextDouble();
+
+        System.out.print("Enter student average marks (0-100): ");
+        double averageMarks = scanner.nextDouble();
+
+        if (attendance >= 75 && averageMarks >= 40) {
+            System.out.println("The student is eligible to take the exam.");
+        } else {
+            System.out.println("The student is not eligible to take the exam.");
+            
+        }
+        scanner.close();
+    }
+}
