60651ad8a9b036f9fc789c60af54cea3e162fb81
Evans mawira <evansmawira2004@gmail.com>
2026-03-16 16:06:48 +0300

    codes debuging

--- Week 1/User_input.java
+++ Week 1/User_input.java
@@ -2,7 +2,7 @@
 //REG NO: CT100/G/21515/24
 import java.util.Scanner;
 
-public class user_input {
+public class User_input {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -17,8 +17,8 @@ public class user_input {
 
         System.out.println();
         System.out.println("--- User Input Credintials ---");
-        System.out.printf("Height:", height);
-        System.out.printf("Bank Balance: KES", balance);
+        System.out.println("Height: " + height);
+        System.out.printf("Bank Balance: KES %.2f%n", balance);
         System.out.println("Phone Number: " + phone);
         System.out.println("------------Thanks for provinding your details--------------");
 
